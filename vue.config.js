bb88b198bcee8c4d182d4baa25e87212481672e8
yang30631 <yang30631@163.com>
2024-12-23 06:48:06 +0000

    1

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
      devServer: {
-         host:"www.uric.cn",
+         allowedHosts: "all",
+         host:"127.0.0.1",
          port: "8080",
      },
 })
